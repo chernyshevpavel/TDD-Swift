56a4c88f4fa45b2e2163bdb65ce5bda4b984a88e
Pavel Chernyshev <titan100695@yandex.ru>
2021-04-28 22:57:42 +0300

    initial

---
+++ ToDoApp/ToDoApp/Controller/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Павел Чернышев on 28.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
