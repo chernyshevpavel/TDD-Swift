363272e5d7330f0d78cf214b2a1394a8e81e89e0
Pavel Chernyshev <titan100695@yandex.ru>
2021-10-10 22:09:14 +0300

    NotificationCenter Test

--- ToDoApp/ToDoAppTests/Controller/TaskListViewControllerTest.swift
+++ ToDoApp/ToDoAppTests/Controller/TaskListViewControllerTest.swift
@@ -86,6 +86,24 @@ class TaskListViewControllerTest: XCTestCase {
         
         XCTAssertTrue((sut.tableView as! MockTableView).isReloaded)
     }
+    
+    func testTappingCellSendsNotification() {
+        let task = Task(title: "Foo")
+        sut.dataProvider.taskManager!.add(task: task)
+        
+        expectation(forNotification: NSNotification.Name(rawValue: "DidSelectRow notification"), object: nil) { notification -> Bool in
+            
+            guard let taskFromNotification = notification.userInfo?["task"] as? Task else {
+                return false
+            }
+            
+            return task == taskFromNotification
+        }
+        
+        let tableView = sut.tableView
+        tableView?.delegate?.tableView!(tableView!, didSelectRowAt: IndexPath(row: 0, section: 0))
+        waitForExpectations(timeout: 1, handler: nil)
+    }
 }
 
 extension TaskListViewControllerTest {
